fb06a9b6d86dd5d7cd3100c0edadc72c9d3f601f
CHicks256 <chicks-student@nfcs.k12.pa.us>
2018-11-02 22:33:25 -0400

    Second Commit
    11/2 10:32 pm
    Prefab for first Clutter item,
    script clutterControl to handle random spawning

---
+++ Collect_the_Clutter/Assets/Scripts/clutterControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clutterControl : MonoBehaviour {
+    public GameObject clutter;
+    public float xBoundSpawn = 7f;
+    public float yBoundSpawn = 7f;
+    public float spawnTimer = 1f;
+    private float spawnXimer;
+	// Use this for initialization
+	void Start () {
+        spawnXimer = spawnTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnXimer -= Time.deltaTime;
+        if (spawnXimer <= 0f)
+        {
+            spawnClutter();
+            spawnXimer = spawnTimer;
+        }
+	}
+    void spawnClutter()
+    {
+        Instantiate(clutter, new Vector3(Random.Range(-xBoundSpawn, xBoundSpawn), Random.Range(-yBoundSpawn, yBoundSpawn), 0f), Quaternion.identity);
+    }
+}
