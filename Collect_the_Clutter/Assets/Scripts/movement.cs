4b3a529e5530f3aae52212bd8faac8d4415377e9
CHicks256 <chicks-student@nfcs.k12.pa.us>
2018-11-02 20:03:18 -0400

    First Commit
     11/2/2018 7:57pm
    Basic movement of a 2D space implemented

---
+++ Collect_the_Clutter/Assets/Scripts/movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public GameObject sprite;
+    public float moveVel = 5f;
+    public float xBound = 7f;
+    public float yBound = 7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < xBound)
+        {
+            transform.position += (Vector3.right * Time.deltaTime) * moveVel;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -xBound)
+        {
+            transform.position += (Vector3.left * Time.deltaTime) * moveVel;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < yBound){
+            transform.position += (Vector3.up * Time.deltaTime) * moveVel;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)&& transform.position.y > -yBound)
+        {
+            transform.position += (Vector3.down * Time.deltaTime) * moveVel;
+        }
+	}
+}
