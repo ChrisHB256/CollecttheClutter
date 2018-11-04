02d7aa8440a6fd8424a099d4740a4d0b4d734f3e
CHicks256 <chicks-student@nfcs.k12.pa.us>
2018-11-04 01:15:28 -0400

    Eight Commit
    11/4 1:10am
    Buttons active, main menu created

---
+++ Collect_the_Clutter/Assets/Scripts/buttonControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void reload()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("GameScene");
+    }
+    public void mainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void exitApp()
+    {
+        Application.Quit();
+    }
+}
