d493f3d15cbf3fe1ae3f28cea8226f1ea5e7d983
CHicks256 <chicks-student@nfcs.k12.pa.us>
2018-11-04 00:37:40 -0400

    Seventh Commit
    11/4 12:35am
    Created a pause menu with buttons "Restart" and "Main Menu," no functionality at this point in time

---
+++ Collect_the_Clutter/Assets/Scripts/panelEsc.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelEsc : MonoBehaviour {
+    public GameObject panel;
+    bool isActive = false;
+
+	// Use this for initialization
+	void Start () {
+        panel.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isActive = !isActive;
+            panel.gameObject.SetActive(isActive);
+            if (isActive)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+	}
+}
